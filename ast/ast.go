ecf6100ef66a144011b01bec1bc7d71eed37242e
Gustavo Pereira <Gustavo.Pereira@flex.com>
2024-02-16 14:06:34 -0300

    Parser: let statements created

--- ast/ast.go
+++ ast/ast.go
@@ -17,12 +17,12 @@ type Expression interface {
 }
 
 type Program struct {
-	Statement []Statement
+	Statements []Statement
 }
 
 func (p *Program) TokenLiteral() string {
-	if len(p.Statement) > 0 {
-		return p.Statement[0].TokenLiteral()
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
