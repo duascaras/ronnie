de3254b7017c25a23dce6e4fe326bbcb1b04367c
duascaras <gustavodasilvapereira14@gmail.com>
2024-02-21 16:59:19 -0300

    Eval boolean literals created

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"ronnie/object"
 )
 
+var (
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -15,6 +20,8 @@ func Eval(node ast.Node) object.Object {
 	//Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}	
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -28,4 +35,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -9,17 +9,33 @@ import (
 
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
-		input string
-		expected int64
-		}{
-			{"5", 5},
-			{"10", 10},
-		}
-		for _, tt := range tests {
-			evaluated := testEval(tt.input)
-			testIntegerObject(t, evaluated, tt.expected)
-		}
+		input 		string 
+		expected 	int64
+	}{
+		{"5", 5},
+		{"10", 10},
 	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input 		string 
+		expected 	bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
 
 func testEval(input string) object.Object {
 	l := lexer.New(input)
@@ -36,6 +52,22 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	}
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%d, want=%d",
+		result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
 		result.Value, expected)
 		return false
 	}
