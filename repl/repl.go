f1c994d14093653dee65527f912e2d9dcd629e1d
duascaras <gustavodasilvapereira14@gmail.com>
2024-02-21 16:36:04 -0300

    Eval integer literals created and eval added to repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"ronnie/evaluator"
 	"ronnie/lexer"
 	"ronnie/parser"
 )
@@ -80,9 +81,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		// io.WriteString(out, OK_FACE + "\n")
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
